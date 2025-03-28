4f09fb25047c5b356772a79ceb48055e6aa6e8f9
YashChauhan1828 <chauhanyash1804@gmail.com>
2025-03-27 20:04:27 -0700

    Add files via upload

---
+++ 24-spring-boot/src/main/java/com/util/Validators.java
@@ -0,0 +1,21 @@
+package com.util;
+
+public class Validators 
+{
+	public static boolean isBlank(String data)
+	{
+		if(data == null || data.trim().length()==0)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+	public static boolean isAlpha(String data)
+	{
+		String alphaRegEx = "[a-zA-Z]+";
+		return data.matches(alphaRegEx);
+	}
+}
